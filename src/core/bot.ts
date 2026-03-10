4c72403caba7accf3ae30b4c1dfde76688cce553
Sherzod Artikbayev <info@web-pro.uz>
2026-03-10 21:33:27 +0500

    fix: production settings

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -4,6 +4,6 @@ import 'dotenv/config'
 const token = process.env.TELEGRAM_BOT_TOKEN
 if (!token) throw new Error('TELEGRAM_BOT_TOKEN is missing')
 
-const bot = new Bot(token)
+export const bot = new Bot(token)
 
 export default webhookCallback(bot, 'std/http')
